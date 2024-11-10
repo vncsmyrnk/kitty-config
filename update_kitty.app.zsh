cadb31c2207f0f70c55981bf63dd5045f62dc802
Vinicius Mayrink <vmayrink12@gmail.com>
2024-11-10 15:50:12 -0300

    feat: adds script for manully updating kitty when needed

---
+++ update_kitty.app.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# Updates manually installed kitty
+
+if [[ $(which kitty) != "/home/$(whoami)/.local/bin/kitty" ]]; then
+  exit 0
+fi
+
+latest_version=$(curl -sL https://api.github.com/repos/kovidgoyal/kitty/releases/latest | jq -r '.tag_name')
+latest_version="${latest_version:1}"
+current_version=$(kitty -v | awk '{ print $2 }')
+
+if [[ "$latest_version" == "$current_version" ]]; then
+  echo "kitty is already on latest version"
+  exit 0
+fi
+
+echo "New version found: $latest_version. Current version is $current_version. New version will be downoaded"
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin dest=~/.local/stow launch=n
+kitty -v
